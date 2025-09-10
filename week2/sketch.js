24dfd0c2ee93be6ab2772e01fec828321e0becde
sh8617 <sh8617@nyu.edu>
2025-09-10 16:55:52 -0400

    123

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,18 @@
 
 
-function setup() {
+function setup() { // runs once at the start
   createCanvas(windowWidth, windowHeight);
-  
+  // creatCanvas is a function
+  // that create a canvas for our p5.js sketch
+  // to draw into. it takes two parameter,
+  // width and height. windowWidth and windowHeight
+  // are used to set the size to the full size
+  // of our browser window.
+
 }
 
-function draw() {
-  
+function draw() { // runs in a loop after setup
+  circle(100,200,25)
+  // circle takes these parameters;
+  // X position, y position, diameter
 }
