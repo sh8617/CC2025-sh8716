3224b1ecb456774a04fcad76314d317b4613a939
sh8617 <sh8617@nyu.edu>
2025-09-17 15:03:24 -0400

    3

--- week2/sketch.js
+++ week2/sketch.js
@@ -8,22 +8,22 @@ function draw() {
 
   // draw a circle
   stroke(0);            // black outline
-  strokeWeight(3);      // outline thickness
+  strokeWeight(5);      // outline thickness
   fill(255, 0, 0);      // red fill
   circle(100, 100, 80); 
   // x, y, diameter
 
   // draw a rectangle
   stroke(0, 0, 255);    // blue outline
-  strokeWeight(2);      
+  strokeWeight(6);      
   fill(0, 255, 0);      // green fill
-  rect(200, 200, 120, 60); 
+  rect(200, 200, 100, 80); 
   // x, y, width, height
 
   // draw an ellipse
   stroke(255, 165, 0);  // orange outline
-  strokeWeight(4);
+  strokeWeight(7);
   fill(255, 255, 0);    // yellow fill
-  ellipse(400, 150, 100, 50); 
+  ellipse(400, 170, 100, 70); 
   // x, y, width, height
 }
--- week3/sketch.js
+++ week3/sketch.js
@@ -1,10 +1,39 @@
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function setup() {// runs once at startup
+  createCanvas(400, 400);// set a 400px by 400ps canvas
   
 }
 
 function draw() {
-  
+  // a grayscale color is denoted as a number 0-255
+  // an rgb color is denoted as 3 numbers (red green blue)
+  // background{127,0,0}
+  // we can use the name of a color like "black" or "olive"
+  // background("aqua")
+
+  background("rgba(180, 114, 114, 1)");
+
+  //stroke and fill change the color of drawn shapes
+  stroke("rgba(167, 55, 55, 0)")
+  fill("rgba(0,0,0,0)")
+
+  strokeWeight(5)
+  //noStroke(): gets rid of the stroke completely
+  //noFill(): gets rid of the fill completely
+
+  //circle takes 3 parameters: x, y and d
+  circle(200,100,50);
+
+  //setting a new fill for m rectangle
+  fill("rgba(201, 65, 65, 0)")
+  //rect takes 4 parameters
+  //x coord of top left, y coord of top left, width and height
+  rect(100,300,150,50)
+  //ellipse takes 4 parameters
+  //x coord of center, y coord of center, width and height
+  ellipse(250,80,10,20)
+
+  //
+  line(250,130,260,130)
 }
