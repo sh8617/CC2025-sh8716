da65a2d92905de1f4be3cc6d3b0732230b082ef3
sh8617 <sh8617@nyu.edu>
2025-09-24 16:46:29 -0400

    class

--- week4/sketch.js
+++ week4/sketch.js
@@ -1,50 +1,42 @@
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 }
 
 function draw() {
-  background("rgba(220, 181, 181, 1)")
-  circle(100,100,100)
-  circle(85,90,5)
-  circle(115,90,5)
-  arc(100,100,60,60,0,PI)
+  background("rgba(220, 181, 181, 1)");
+  circle(100, 100, 100);
+  circle(85, 90, 5);
+  circle(115, 90, 5);
+  arc(100, 100, 60, 60, 0, PI);
   //translate is a Transformation function
   // it moves the coordinate matrix according to
   //a new set of coordinates, which
   //the "new" 0,0
- 
+
   //push and pop isolate a transformation
   //anything enclosed within push and pop only applies
   //within that enclosure
-  push()// push indicates the beginning of an isolate
-  fill("pink")
-  translate(100,100)
-  circle(0,0,100)
-  circle(100,100,100)
-  circle(85,90,5)
-  circle(115,90,5)
-  arc(100,100,60,60,0,PI)
-  pop()// pop indicates the end of an isolated 
+  push(); // push indicates the beginning of an isolate block
+  fill("pink");
+  translate(100, 100);
+  circle(0, 0, 100);
+  circle(100, 100, 100);
+  circle(85, 90, 5);
+  circle(115, 90, 5);
+  arc(100, 100, 60, 60, 0, PI);
+  pop(); // pop indicates the end of an isolated block
   //text function； text, x, y off top left corner
-  angle = map(mouseX,0,width,0,360)
-  push()
-  rotate(radians(mouseX))
-  strokeWeight(4)
-  stroke(0)
-  line(0,0,100,0)
-  pop()
-
-  let angle = map(mouseX, 0, width, 0, 360); // 映射到 0-360 度
-
+  angle = map(mouseX, 0, width, 0, 360);
   push();
-  translate(width/2, height/2); // 把旋转中心移到画布中心
-  rotate(radians(angle));       // 根据鼠标位置旋转
+  rotate(radians(mouseX));
   strokeWeight(4);
   stroke(0);
-  line(0, 0, 100, 0);           // 画一条指针
+  line(0, 0, 100, 0);
   pop();
-  text(mousenX + "," + mouseY,5,15)
+
+  if (condition) {
+  } else {
+  }
+
+  text(mousenX + "," + mouseY, 5, 15);
 }
